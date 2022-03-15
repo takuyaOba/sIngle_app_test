8514d1377f94e1c1e2b5a91addfa2eb89268d3d8
takuyaOba <49937871+takuyaOba@users.noreply.github.com>
2022-03-15 23:22:47 +0900

    Initial Commit

---
+++ single_app_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  single_app_test
+//
+//  Created by mt-square.toba on 2022/03/15.
+//  Copyright © 2022 takuyaOba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ single_app_testTests/single_app_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  single_app_testTests.swift
+//  single_app_testTests
+//
+//  Created by mt-square.toba on 2022/03/15.
+//  Copyright © 2022 takuyaOba. All rights reserved.
+//
+
+import XCTest
+@testable import single_app_test
+
+class single_app_testTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
