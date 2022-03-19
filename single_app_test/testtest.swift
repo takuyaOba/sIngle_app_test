537cbebeea5d26b4ce377f1a6fd01969a3d4445f
takuyaOba <49937871+takuyaOba@users.noreply.github.com>
2022-03-19 22:59:42 +0900

    testtest

---
+++ single_app_test/testtest.swift
@@ -0,0 +1,10 @@
+//
+//  testtest.swift
+//  single_app_test
+//
+//  Created by mt-square.toba on 2022/03/19.
+//  Copyright © 2022 takuyaOba. All rights reserved.
+//
+
+import Foundation
+//testetst
